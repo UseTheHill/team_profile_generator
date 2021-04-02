bdd8aeced8dc99f390a1c96c2bd8a687dcba1eca
Kayte McDonough <kaytemcdonough@gmail.com>
2021-04-02 10:40:21 -0400

    added specifics and basics for command line questions

--- app.js
+++ app.js
@@ -14,6 +14,57 @@ let teamMembers = [];
 
 let employeeBasics = [
     {
-        
-    }
-]
+        type: 'list',
+        message: 'Which type of team member would you like to add?',
+        choices: ['Manager', 'Engineer', 'Intern'],
+        name: 'employeeType',
+    },
+    {
+        type: 'input',
+        message: "Employee's Name",
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: "Employee's Id",
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: "Employee's Email",
+        name: 'email',
+    },
+];
+
+let managerSpecifics = [
+    {
+        type: 'input',
+        message: "Office Number",
+        name: 'officeNumber',
+    },
+];
+
+let engineerSpecifics = [
+    {
+        type: 'input',
+        message: "Github username",
+        name: 'githubUsername',
+    },
+];
+
+let internSpecifics = [
+    {
+        type: 'input',
+        message: "School",
+        name: 'school',
+    },
+];
+
+let verify = [
+    {
+        type: 'confirm',
+        message: "Want to add another employee? Hit enter for YES",
+        name: 'verify',
+        default: true,
+    },
+];
